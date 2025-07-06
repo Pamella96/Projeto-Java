5858462fa942589104e2b8c463d6055c3d12d7fc
mooncake96 <72810087+Pamella96@users.noreply.github.com>
2025-07-05 23:32:10 -0300

    Primeiro commit

---
+++ src/modelos/Produto.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package modelos;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import enums.StatusAvaliacao;
+
+/**
+ * 
+ */
+public abstract class Produto implements Comparable<Produto> {
+    protected String nome;
+    protected String marca;
+    protected List<Avaliacao> avaliacoes = new ArrayList<>();
+
+    public Produto() {
+
+    }
+
+    /**
+     * @param nome
+     * @param marca
+     * @param avaliacoes
+     */
+    public Produto(String nome, String marca) {
+        this.nome = nome;
+        this.marca = marca;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Avaliacao> getAvaliacoes() {
+        return Collections.unmodifiableList(avaliacoes);
+    }
+
+    public void adicionarAvaliacao(Avaliacao avaliacao) {
+        avaliacoes.add(avaliacao);
+    }
+
+    public double calcularNotaMedia() {
+        return avaliacoes.stream()
+                .filter(avaliacao -> avaliacao.getStatus() == StatusAvaliacao.APROVADA)
+                .mapToInt(Avaliacao::getNota)
+                .average()
+                .orElse(0);
+    }
+
+    public abstract String exibirDetalhes(); // toString
+
+    @Override
+    public int compareTo(Produto outro) {
+        return this.nome.compareToIgnoreCase(outro.nome);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Produto p))
+            return false;
+        return Objects.equals(nome, p.nome) && Objects.equals(marca, p.marca);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, marca);
+    }
+}
---
+++ src/modelos/Usuario.java
@@ -0,0 +1,120 @@
+/**
+ * 
+ */
+package modelos;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import enums.StatusUsuario;
+import interfaces.Moderacao;
+
+/**
+ * 
+ */
+public class Usuario implements Moderacao {
+    private String nome;
+    private String email;
+    private StatusUsuario status;
+    private List<Avaliacao> avaliacoesFeita = new ArrayList<Avaliacao>();
+
+    /**
+     * @param nome
+     * @param email
+     * @param status
+     * @param avaliacoesFeita
+     */
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+        this.status = status.PENDENTE_APROVACAO;
+    }
+
+    /**
+     * 
+     */
+    public Usuario() {
+
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome1() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the status
+     */
+    public StatusUsuario getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(StatusUsuario status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the avaliacoesFeita
+     */
+    public List<Avaliacao> getAvaliacoesFeita() {
+        return avaliacoesFeita;
+    }
+
+    /**
+     * @param avaliacoesFeita the avaliacoesFeita to set
+     */
+    public void setAvaliacoesFeita(List<Avaliacao> avaliacoesFeita) {
+        this.avaliacoesFeita = avaliacoesFeita;
+    }
+
+    public void avaliarProduto(Produto produto, int nota, String texto) {
+        Avaliacao a = new Avaliacao(nota, texto, this, produto);
+        avaliacoesFeita.add(a);
+        produto.adicionarAvaliacao(a);
+    }
+
+    @Override
+    public void aprovar() {
+        this.status = StatusUsuario.ATIVO;
+    }
+
+    @Override
+    public void rejeitar() {
+        this.status = StatusUsuario.BLOQUEADO;
+    }
+
+    public List<Avaliacao> getAvaliacoesFeitas() {
+        return Collections.unmodifiableList(avaliacoesFeita);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
